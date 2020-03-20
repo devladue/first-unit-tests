90314f985eb09e758ec6f6d71db489a2378cb123
Dustin LaDue <dustin.ladue@yahoo.com>
2020-03-19 18:33:10 -0600

    Add code to tests

--- functions.js
+++ functions.js
@@ -1,3 +1,32 @@
 // functions.js
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function sayHello(name = "Will") {
+  greet1 = "Hi there " + name + "!";
+  greet2 = "No more testing Will!";
+  if(name === "Will") {
+    return greet2;
+  }
+  return greet1;
+
+}
+
+function buildCar(color, type) {
+  if(!color && !type){
+    return{};
+  } else {
+  return {color, type};
+  }
+}
+
+var color = buildCar("red");
+var type = buildCar("truck");
 
 
+module.exports={
+  addTwoNumbers: addTwoNumbers,
+  sayHello: sayHello,
+  buildCar: buildCar,
+}
